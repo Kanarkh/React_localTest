4682e1969d050cf2ebd9e74e415b665a71421618
Kanarkh <55624991+Kanarkh@users.noreply.github.com>
2020-06-01 18:16:46 +0900

    react_componet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("hello");
+    }
+    state = {
+        count: 0
+    };
+    add = () => {
+        console.log("add");
+        //setState를 실행할때마다 새로운 count값을 가지고 render를 실행시킨다
+        //
+        this.setState(current => ({count: current.count + 1}));
+    };
+    minus = () => {
+        console.log("minus");
+        // this.state.count -= 1;
+        this.setState(test => ({count: test.count - 1}));
+    };
+
+    render() {
+        console.log("componet rendering")
+        return (
+            <div>
+                <h1>The number is {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        )
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("updated")
+    }
+
+    componentDidMount() {
+        console.log("component did mount")
+    }
+    componentWillUnmount() {
+        console.log("Goodbye, world");
+    }
 }
 
 export default App;
